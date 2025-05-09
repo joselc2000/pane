ce3be520efd5bca08f54cfb977d203ca8c853561
joselc2000 <20243rd042@utez.edu.mx>
2025-05-09 09:29:54 -0600

    Apellidos

--- Entrada.java
+++ Entrada.java
@@ -4,5 +4,8 @@ public class Entrada {
     public static void main(String[] args){
         String nombreUsuario = JOptionPane.showInputDialog("Ingresa tu nombre: ");
         System.out.println("Tu nombre es " + nombreUsuario);
+
+        String apellidosUsuario = JOptionPane.showInputDialog("Ingresa tu apedillo: ");
+        System.out.println("Tu apellido es " + apellidosUsuario);
     }
 }
